be9aad2647fd7c8b476f87a80fa2052d56b7db6e
AbdelrahmanSuliman <abdelrahmansuliman123@gmail.com>
2026-06-26 14:59:11 +0300

    chore: add 2 new error types AppError & AuthenticationError

---
+++ src/util/error.ts
@@ -0,0 +1,14 @@
+export class AppError extends Error {
+  statusCode: number;
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    this.name = this.constructor.name
+  }
+}
+
+export class AuthenticationError extends AppError {
+  constructor(message: string = "Invalid Email or Password") {
+      super(message, 401)
+  }
+}
