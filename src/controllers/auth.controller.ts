af6aaa8d76330924e1a445904c3c0510131f1645
AbdelrahmanSuliman <abdelrahmansuliman123@gmail.com>
2026-06-28 17:28:58 +0300

    feat: add logic to login and sign up services & controllers

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,3 +1,32 @@
-export function signupController() {}
+import type { NextFunction, Request, Response } from "express";
+import { signupService, loginService } from "../services/auth.services";
+import { StatusCodes } from "http-status-codes";
 
-export function loginController() {}
+export async function signupController(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const { username, email, password } = req.body;
+
+  try {
+    const user = await signupService(username, email, password);
+    res.status(StatusCodes.CREATED).send({ user });
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function loginController(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const { email, password } = req.body;
+  try {
+    const user = await loginService(email, password)
+    res.status(StatusCodes.OK).send({user})
+  } catch (err) {
+    next(err)
+  }
+}
--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -4,28 +4,39 @@ import db from "../db/index";
 import * as t from "../db/schema";
 import bcrypt from "bcrypt";
 import config from "../config/index";
+import logger from "../util/logger";
 
 export async function signupService(
   username: string,
   email: string,
   password: string,
 ) {
+  const saltRounds = Number(config.saltRounds);
   try {
-    const passwordHash = await bcrypt.hash(password, config.saltRounds);
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+    const user = await db.query.users.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (user) throw new AuthenticationError("User already exists");
+
     const [newUser] = await db
       .insert(t.users)
       .values({ username, email, passwordHash })
       .returning({
-        id: t.users.id,
-        username: t.users.username,
-        email: t.users.email,
+        id: users.id,
+        username: users.username,
+        email: users.email,
       });
+
     return newUser;
   } catch (err) {
     if (err instanceof AuthenticationError) {
       throw err;
     } else {
-      throw new AppError("Something went wrong during login", 500);
+      throw new AppError("Something went wrong during signup", 500);
     }
   }
 }
@@ -36,13 +47,24 @@ export async function loginService(email: string, password: string) {
       where: {
         email,
       },
+      columns: {
+        id: true,
+        email: true,
+        passwordHash: true,
+      },
     });
 
     if (!user) throw new AuthenticationError();
+
     const isPasswordValid = await bcrypt.compare(password, user.passwordHash);
     if (!isPasswordValid) {
       throw new AuthenticationError();
     }
+
+    return {
+      id: user.id,
+      email: user.email,
+    };
   } catch (err) {
     if (err instanceof AuthenticationError) {
       throw err;
