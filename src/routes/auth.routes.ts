ebf650cd3c9598177c223d9133f0bb299553b72d
AbdelrahmanSuliman <abdelrahmansuliman123@gmail.com>
2026-06-30 14:54:43 +0300

    feat: add validation middleware to auth routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { loginController, signupController } from "../controllers/auth.controller";
+import { validateData } from "../middleware/validation.middleware";
+import {userLoginSchema, userSignupSchema} from '../schema/auth.schema'
 
 const authRouter = express.Router();
 
-authRouter.post("/signup", signupController);
-authRouter.post("/login", loginController)
+authRouter.post("/signup", validateData(userSignupSchema), signupController);
+authRouter.post("/login", validateData(userLoginSchema), loginController)
 
 export default authRouter
