bbf6d179110a7a38b8ddcc39301638cdc5d86129
AbdelrahmanSuliman <abdelrahmansuliman123@gmail.com>
2026-07-13 12:33:40 +0300

    chore: move parsing config schema from server entry to config file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,17 +1,18 @@
-import dotenv from "dotenv";
-dotenv.config();
+import ConfigSchema from "../schema/config";
+
 
 export const config = {
   port: process.env.PORT,
   database: {
     url: process.env.DATABASE_URL,
   },
-  saltRounds: process.env.SALT_ROUNDS ,
+  saltRounds: process.env.SALT_ROUNDS,
   jwt: {
-    expiresIn: process.env.JWT_EXPIRATION_TIME ,
-    secretKey: process.env.JWT_SECRET as any
-  }
+    expiresIn: process.env.JWT_EXPIRATION_TIME,
+    secretKey: process.env.JWT_SECRET,
+  },
 };
 
+ConfigSchema.parse(config);
 
-export default config
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+import "dotenv/config";
 import express from "express";
 import helmet from "helmet";
 import config from "./config/index";
@@ -5,22 +7,20 @@ import logger from "./util/logger";
 import errorHandler from "./middleware/error.handler";
 import authRouter from "./routes/auth.routes";
 import addictionRouter from "./routes/addiction.routes";
-import ConfigSchema from "./schema/config";
-const app = express();
 
+const app = express();
+dotenv.config();
 //Global Middleware
 app.use(helmet());
 app.use(express.json());
 
 //Router Mounting
 const router = express.Router();
-router.use("/auth", authRouter)
-router.use("/addiction", addictionRouter )
+router.use("/auth", authRouter);
+router.use("/addiction", addictionRouter);
 app.use("/api/v1", router);
 
 app.use(errorHandler);
-
-ConfigSchema.parse(config)
 
 app.listen(config.port, () => {
   logger.info(`Server is running on port ${config.port}`);
