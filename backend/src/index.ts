dc09dae9e28fac28a5aa73b95f3f0f37f8f8faf7
AbdelrahmanSuliman <abdelrahmansuliman123@gmail.com>
2026-08-13 21:57:55 -0600

    chore: setup tsup bundler

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
-import dotenv from "dotenv";
 import "dotenv/config";
+import dotenv from "dotenv";
 import express from "express";
 import helmet from "helmet";
 import config from "./config/index";
@@ -8,20 +8,22 @@ import errorHandler from "./middleware/error.handler";
 import authRouter from "./routes/auth.routes";
 import addictionRouter from "./routes/addiction.routes";
 import journalRouter from "./routes/journal.routes";
-import invitationRouter from './routes/invitation.routes'
+import invitationRouter from "./routes/invitation.routes";
+import limiter from "./middleware/limiter.middleware";
 
 const app = express();
 dotenv.config();
 //Global Middleware
 app.use(helmet());
+app.use(limiter);
 app.use(express.json());
 
 //Router Mounting
 const router = express.Router();
 router.use("/auth", authRouter);
 router.use("/addiction", addictionRouter);
-router.use("/journal", journalRouter)
-router.use("/invitation", invitationRouter)
+router.use("/journal", journalRouter);
+router.use("/invitation", invitationRouter);
 app.use("/api/v1", router);
 
 app.use(errorHandler);
